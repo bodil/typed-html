83360f190802d165d2fd8a6419c08f1c0b8d17b6
Vincent Khougaz <vincent@khougaz.com>
2018-12-15 18:22:49 -0800

    Hinting mime type to node so firefox is willing to load it

--- examples/stdweb/www/index.js
+++ examples/stdweb/www/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var pathToBuild = process.argv[2] || '../../../target/wasm32-unknown-unknown/release';
 
+express.static.mime.define({'application/wasm': ['wasm']});
+
 var app = express();
 
 app.use(express.static(path.resolve(pathToBuild), { index: false }));
