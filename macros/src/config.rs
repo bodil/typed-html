9a3b184b0d3ee4b32ec4d2787459d7f9c5871381
Weiyi Wang <wwylele@gmail.com>
2022-11-08 09:56:14 -0500

    Add global attribute itemscope/itemtype

--- macros/src/config.rs
+++ macros/src/config.rs
@@ -28,6 +28,8 @@ pub fn global_attrs(span: Span) -> StringyMap<Ident, TokenStream> {
         insert("hidden", "crate::types::Bool");
         insert("is", "String");
         insert("itemprop", "String");
+        insert("itemscope", "crate::types::Bool");
+        insert("itemtype", "String");
         insert("lang", "crate::types::LanguageTag");
         insert("role", "crate::types::Role");
         insert("style", "String");
